4c8f970f270c5dc28ef03f647854acb8f486bf5e
Tadeuvelloso <tadeuvellososilva@gmail.com>
2023-06-19 15:52:15 -0300

    feat: users repository

---
+++ src/repository/userRepository.ts
@@ -0,0 +1,26 @@
+import prisma from "../database/db.js"
+import { User } from "../protocols/User.js"
+
+
+async function newUser(user: User) {
+    return await prisma.users.create({
+        data: {
+            email: user.email,
+            name: user.name,
+            password: user.password
+        }
+    })
+}
+
+async function findUserByEmail(email: string) {
+    return await prisma.users.findFirst({
+        where: {
+            email
+        }
+    })
+}
+
+export {
+    newUser,
+    findUserByEmail
+}
